1ccbed2fff30ed86883c27fdbe026c71c3679030
Samuel Castro <dreift133@gmail.com>
2024-12-16 20:50:16 +0100

    renderizando en react con una cadena de texto

---
+++ PRUEBA.js
@@ -0,0 +1,10 @@
+alert("¡Hola, mundo! Este es un mensaje de alerta.");
+
+"use strict"; 
+import React from "http://esm.sh/react@18.2.0";
+import ReactDOM from "http://esm.sh/react-dom@18.2.0/client"; 
+
+const appDOMelement = document.getElementById('app');
+const root = ReactDOM.createRoot(appDOMelement);
+
+root.render('hey');
