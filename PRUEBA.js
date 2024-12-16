a43c5273eba65e41c4378cf7008697e7aaa8b977
Samuel Castro <dreift133@gmail.com>
2024-12-16 21:42:58 +0100

    instalacion de vit

--- PRUEBA.js
+++ PRUEBA.js
@@ -1,4 +1,4 @@
-alert("¡Hola, mundo! Este es un mensaje de alerta.");
+
 
 "use strict"; 
 import React from "http://esm.sh/react@18.2.0";//importar react y reactdom
@@ -10,6 +10,6 @@ const button = React.createElement('button',{"data-id":123},'Button1')//para cre
 //ahora mismo al button estar recibiendo una propiedad que es la dataid, tambien podria ser una classname
 const button2 = React.createElement('button',{"data-id":456},'Button2')
 const button3= React.createElement('button',{"data-id":789},'Button3')   
-
-const div= React.createElement('div',null,[button,button2,button3])
-root.render(div);
+//para renderizar mas de un elemento a la vez creamos un cuarto que sera un div este tendra dentro un array
+const div= React.createElement(React.Fragment,null,[button,button2,button3])
+root.render(div);//renderizamos el div.Pero si no queremos renderizar un div podemos optar por esta opcion
