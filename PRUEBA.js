051f65a80a21a0926b0da58b7a40360a40de98b1
Samuel Castro <dreift133@gmail.com>
2024-12-16 21:11:34 +0100

    renderizacion de un elemento con propiedades y varios a la vez con el uso de div

--- PRUEBA.js
+++ PRUEBA.js
@@ -6,5 +6,10 @@ import ReactDOM from "http://esm.sh/react-dom@18.2.0/client";
 
 const appDOMelement = document.getElementById('app');
 const root = ReactDOM.createRoot(appDOMelement);//creamos un un root para renderizar algo en este caso un mensaje
-const button = React.createElement('button',null,'Me gusta')//para crear un elemento los tres parametros serian el elemento,propiedades y lo que hay dentro del elemento
-root.render(button);
+const button = React.createElement('button',{"data-id":123},'Button1')//para crear un elemento los tres parametros serian el elemento,propiedades y lo que hay dentro del elemento
+//ahora mismo al button estar recibiendo una propiedad que es la dataid, tambien podria ser una classname
+const button2 = React.createElement('button',{"data-id":456},'Button2')
+const button3= React.createElement('button',{"data-id":789},'Button3')   
+
+const div= React.createElement('div',null,[button,button2,button3])
+root.render(div);
