a69b4779f2f57b54008d86398dcbfbe0337ed60b
Samuel Castro <dreift133@gmail.com>
2024-12-23 23:04:36 +0100

    renderizacion de botones en jsx con react.fragment

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.Fragment>
+<button>hola mundo</button>
+<button>hola mundo</button>
+  <button>hola mundo</button>  {// Creamos varios botones usando jsx y renderizandolo a la vez con react.fragment
+      }
+  </React.Fragment>
+
 )
