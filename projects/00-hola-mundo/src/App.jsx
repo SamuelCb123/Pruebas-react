133983fe2dd9492acab4309123ab22676cfa2087
Samuel Castro <dreift133@gmail.com>
2025-01-06 17:03:04 +0100

    proyecto realizado siguiendo el curso midudeev

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -2,32 +2,40 @@ import React, { useState } from 'react';
 import './App.css';
 import { TwitterFollowCard } from './TwitterFollowCard';
 
+// Array de usuarios
+const users = [
+    {
+        UserName: 'Antonio',
+        name: 'Antonio Banderas',
+        isFollowing: true,
+    },
+    {
+        UserName: 'saiken',
+        name: 'Saiken Sonkaru',
+        isFollowing: false,
+    },
+    {
+        UserName: 'goku',
+        name: 'Son Goku',
+        isFollowing: true,
+    },
+];
 
 export function App() {
-    
- const [name, setname] = useState('carlos')
- const cambiar = () => {
-setname('aas'); // Cambia elnombre al pulsar el boton
-};
     return (
-        <section className='App'>
-            <TwitterFollowCard   UserName= {name} InitialisFollowing={true}  >
-                Carlos Nogeroles
-                
-                </TwitterFollowCard> 
-             
-            
-            <TwitterFollowCard  UserName="saiken">
-                Saiken sonkaru
-            </TwitterFollowCard>
-            
-            <TwitterFollowCard  UserName="goku">
-                Son Goku
-            </TwitterFollowCard>
-           
-         
-            <button onClick={cambiar}>Cambio de nombre</button> 
+        <section className="App">
+            {users.map((user) => {
+                const { UserName, name, isFollowing } = user;
+                return (
+                    <TwitterFollowCard
+                        key={UserName} 
+                        UserName={UserName}
+                        InitialisFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
+                );
+            })}
         </section>
     );
-
 }
