7e2a3dc89840904a7b1ba8eb6d868c5d3ba093bc
Samuel Castro <dreift133@gmail.com>
2025-01-09 13:45:00 +0100

    mostrammos la posicion de cada celda en el tablero

--- projects/02-Tres-en-raya/src/App.jsx
+++ projects/02-Tres-en-raya/src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
- 
+  const turns = {
+    X: 'x',
+    o: 'O', // Creación de constantes para los turnos
+  };
+
+  const board = Array(9).fill(null); // Array de 9 posiciones para el tablero
 
   return (
-    <>
-     <h1>Tres en raya</h1>
-    </>
-  )
+    <main className="board">
+      <h1>Tres en Raya</h1>
+      <section className="game">
+        {
+          board.map((_, index) => (
+            <div className="cell" >
+              {/* Cada celda tiene un identificador único */}
+              <span className="cell_content">
+                {index} {/* Mostramos el índice de cada celda */}
+              </span>
+            </div>
+          ))
+        }
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
